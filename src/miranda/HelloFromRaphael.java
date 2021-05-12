4553a7fc96cd53307f1508e378f1ed51258dc602
rphl0329 <rphl.mrnda767@gmail.com>
2021-05-13 06:58:34 +0800

    Initial commit

---
+++ src/miranda/HelloFromRaphael.java
@@ -0,0 +1,9 @@
+package miranda;
+
+public class HelloFromRaphael {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello From Raphael!");
+    }
+}
