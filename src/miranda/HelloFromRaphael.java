f0135873a3f8cf668f4ef05860d973c75e0c647b
rphl0329 <rphl.mrnda767@gmail.com>
2021-05-13 07:21:25 +0800

    add team members

--- src/miranda/HelloFromRaphael.java
+++ src/miranda/HelloFromRaphael.java
@@ -4,6 +4,8 @@ public class HelloFromRaphael {
 
     public static void main(String[] args) {
 	// write your code here
+
         System.out.println("Hello From Raphael!");
+        System.out.println("Hello From Team Members");
     }
 }
